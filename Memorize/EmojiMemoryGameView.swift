acf97301bf252c4e477f094b66abd4f1fe0f9a1d
donggeon Na <ndmb2012@gmail.com>
2024-08-17 16:17:36 +0900

    feat: emoji memorize game view

--- Memorize/EmojiMemoryGameView.swift
+++ Memorize/EmojiMemoryGameView.swift
@@ -7,124 +7,60 @@
 import SwiftUI
 
 struct EmojiMemoryGameView: View {
-    @ObservedObject var viewModel: EmojiMemorizeGame
+    @EnvironmentObject var themeStore: ThemeStore
+    @ObservedObject var emojiMemorizeGame: EmojiMemorizeGame
+    
+    init(theme: MemorizeTheme) {
+        emojiMemorizeGame = EmojiMemorizeGame(theme: theme)
+    }
     
     var body: some View {
-        VStack{
-            title
-            cards
-                .animation(.default, value: viewModel.cards)
-            stat
-            newGame
-        }
-    }
-    
-    var title: some View {
-        Text("Memorize!")
-            .font(.largeTitle)
-            .padding()
-    }
-    
-    var cards: some View {
-        ScrollView {
+        GeometryReader { geometry in
+            let itemWidth: CGFloat = cardWidth(cardsCount: emojiMemorizeGame.cards.count, in: geometry, aspect: 2/3)
+            
             LazyVGrid(
-                columns: [GridItem(.adaptive(minimum: 85), spacing: 0)],
-                spacing: 0) {
-                    ForEach(viewModel.cards){ card in
-                        CardView(
-                            card: card,
-                            colorTheme: ColorConverter.convert(viewModel.colorTheme)
-                        )
-                        .padding(4)
-                        .foregroundColor(ColorConverter.convert(viewModel.colorTheme))
-                        .onTapGesture {
-                            viewModel.choose(card)
-                        }
+                columns: [GridItem(.adaptive(minimum: itemWidth), spacing: 0)],
+                spacing: 0
+            ) {
+                ForEach(emojiMemorizeGame.cards) { card in
+                    view(for: card)
+                        .padding(5)
                 }
             }
-            .padding()
         }
+        .padding()
     }
     
-    var stat: some View {
-        HStack{
-            Text("Score \(viewModel.score)")
-            Spacer()
-            Text(viewModel.themeName)
-        }.padding()
-    }
-    
-    var newGame: some View {
-        Button(action: {
-            viewModel.newGame()
-        }, label: {
-            VStack(spacing: 10) {
-                Image(systemName: "gamecontroller.fill")
-                    .imageScale(.medium)
-                    .font(.title)
-                Text("New Game")
-            }
-        })
-    }
-    
-}
-
-struct CardView: View {
-    var card: MemorizeGame<String>.Card
-    var colorTheme: Color
-    
-    init(card: MemorizeGame<String>.Card, colorTheme: Color) {
-        self.card = card
-        self.colorTheme = colorTheme
-    }
-    
-    var body: some View {
+    func view(for card: Card) -> some View {
         ZStack {
-            let base = RoundedRectangle(cornerRadius: 12)
-            
-            base.fill(.white)
+            let base = RoundedRectangle(cornerRadius: 15)
             base.stroke(lineWidth: 2)
-            card.isFaceUp ? (
-                Text(card.content)
-                    .font(.system(size: 200))
-                    .minimumScaleFactor(0.001)
-                    .aspectRatio(1, contentMode: .fit)
-            ) : nil
-                
-            base.fill(
-                LinearGradient(
-                    colors: [colorTheme, .white],
-                    startPoint: UnitPoint(),
-                    endPoint: UnitPoint(x: 1, y: 1))
-            ).opacity(card.isFaceUp ? 0 : 1)
-            
+            Text(card.content)
+                .font(.system(size: 1000))
+                .minimumScaleFactor(0.001)
+                .aspectRatio(1, contentMode: .fit)
+            base.fill(card.isFaceUp ? .clear : .green)
         }
-        .aspectRatio(2/3, contentMode: /*@START_MENU_TOKEN@*/.fill/*@END_MENU_TOKEN@*/)
-        .opacity(card.isMatch ? 0 : 1)
+        .aspectRatio(2/3, contentMode: .fit)
+        .foregroundColor(.green)
+    }
+    
+    func cardWidth(cardsCount: Int, in geometry: GeometryProxy, aspect: CGFloat = 2/3) -> CGFloat {
+        var numberOfCol: CGFloat = 1
+        var numberOfRow: CGFloat = CGFloat(cardsCount) / numberOfCol
+        var cardHeight = geometry.size.width / aspect
+        while (CGFloat(numberOfRow) * cardHeight) > geometry.size.height {
+            numberOfCol += 1
+            numberOfRow = (CGFloat(cardsCount) / numberOfCol).rounded(.up)
+            let cardWidth = geometry.size.width / CGFloat(numberOfCol)
+            cardHeight = cardWidth / aspect
+        }
+        return cardHeight * aspect
     }
 }
 
-struct ColorConverter {
-    static func convert(_ color: String) -> Color {
-        switch(color) {
-        case "red":
-            return .red
-        case "pink":
-            return .pink
-        case "grey":
-            return .gray
-        case "blue":
-            return .blue
-        case "green":
-            return .green
-        case "purple":
-            return .purple
-        default:
-            return .black
-        }
-    }
-}
+
 
 #Preview {
-    EmojiMemoryGameView(viewModel: EmojiMemorizeGame())
+    EmojiMemoryGameView(theme: ThemeStore(name: "Preview").themes.first!)
 }
